4b6e8226cf54507985947f3b6d0515ba423ae088
linhle <linhle@Hieus-MacBook-Air.local>
2020-07-13 18:01:01 +0800

    Updates

---
+++ precache-manifest.52282c860d79f3b177f9e92d80499b1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d015068a58a0d68a3dc5870dabb6fc",
+    "url": "/Grocery-react-app/index.html"
+  },
+  {
+    "revision": "f3ba918af36639959c80",
+    "url": "/Grocery-react-app/static/css/main.15791852.chunk.css"
+  },
+  {
+    "revision": "b16bfdc22a20afa39073",
+    "url": "/Grocery-react-app/static/js/2.20e6a687.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery-react-app/static/js/2.20e6a687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ba918af36639959c80",
+    "url": "/Grocery-react-app/static/js/main.4a8ea9b3.chunk.js"
+  },
+  {
+    "revision": "b744851f5b421888b066",
+    "url": "/Grocery-react-app/static/js/runtime-main.4aa2b921.js"
+  }
+]);
